2ae957f7a289bdb9d0ad8e1384e99afcf9e871ba
Hubert Chang <0417csj@gmail.com>
2025-03-19 16:21:03 +0800

    update openroad package repo

--- var/spack/repos/openroad_dev/packages/openroad/package.py
+++ var/spack/repos/openroad_dev/packages/openroad/package.py
@@ -1,3 +1,4 @@
+import spack.build_systems.cmake
 from spack.package import *
 
 
@@ -38,7 +39,7 @@ class Openroad(CMakePackage):
     depends_on("libffi")
     depends_on("llvm")
     depends_on("lemon")
-    depends_on("or-tools@9.11:")
+    depends_on("or-tools")
     depends_on("glpk")
     depends_on("zlib")
     depends_on("clp")
@@ -55,15 +56,6 @@ class CMakeBuilder(spack.build_systems.cmake.CMakeBuilder):
         args = [
             "-DCMAKE_BUILD_TYPE=RELEASE",
             "-DCMAKE_INSTALL_PREFIX={0}".format(self.spec.prefix),
-            "-Dbison_ROOT={0}".format(self.spec["bison"].prefix),
-            "-DSWIG_ROOT={0}".format(self.spec["swig"].prefix),
-            "-DBoost_ROOT={0}".format(self.spec["boost"].prefix),
-            "-DEigen3_ROOT={0}".format(self.spec["eigen"].prefix),
-            "-DLEMON_ROOT={0}".format(self.spec["lemon"].prefix),
-            "-Dspdlog_ROOT={0}".format(self.spec["spdlog"].prefix),
-            "-DGTest_ROOT={0}".format(self.spec["googletest"].prefix),
-            "-Dortools_ROOT={0}".format(self.spec["or-tools"].prefix),
-
         ]
         
         if "+gui" in self.spec:
